9e0df951273884652bd5648156771e8fe6c8f642
ariannanicasio <ariannanicasio2@gmail.com>
2023-09-25 11:45:04 -0700

    finished classes

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,14 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class JobTest {
-    //TODO: Create your unit tests here
+    //TODO: Create your unit tests
+    @Test
+    public void testSettingJobId() {
+      Job testJob1 = new Job();
+      Job testJob2 = new Job();
+    Assert.assertNotEquals(testJob1, testJob2);
+    }
 }
