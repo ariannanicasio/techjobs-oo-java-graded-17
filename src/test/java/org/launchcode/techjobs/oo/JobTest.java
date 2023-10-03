9a6aedb43c1dd6a269945a2601a0cf3c5bc3e2ae
ariannanicasio <ariannanicasio2@gmail.com>
2023-10-02 21:32:44 -0700

    testing job test

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -11,4 +11,41 @@ public class JobTest {
       Job testJob2 = new Job();
     Assert.assertNotEquals(testJob1, testJob2);
     }
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+        Assert.assertEquals(testJob.getName(), "Product tester");
+        Assert.assertEquals(testJob.getEmployer().getValue(), "ACME");
+        Assert.assertEquals(testJob.getLocation().getValue(), "Desert");
+        Assert.assertEquals(testJob.getPositionType().getValue(), "Quality control");
+        Assert.assertEquals(testJob.getCoreCompetency().getValue(), "Persistence");
+        Assert.assertTrue(testJob.getEmployer() instanceof Employer);
+        Assert.assertTrue(testJob.getLocation() instanceof Location);
+        Assert.assertTrue(testJob.getPositionType() instanceof PositionType);
+        Assert.assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+    }
+    @Test
+    public void testJobsForEquality() {
+        Job testJob = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+        Job testJob2 = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+        Assert.assertFalse(testJob.equals(testJob2));
+    }
 }
